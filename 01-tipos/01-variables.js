0a9058ed29bb4f50fe91fa29a07390a658d8c100
Rimelin <medinaricardob@gmail.com>
2024-01-26 21:49:16 -0600

    Initial commit

---
+++ 01-tipos/01-variables.js
@@ -0,0 +1,10 @@
+//UpperCamelCase, camelCase , snake_case
+//usaremos camelCase
+let nombre = "Hola Mundo";
+let NombreCompleto;
+let nombreCompleto; 
+let nombre_completo;
+console.log(nombre);
+
+let apellido;
+apellido = "Schurmann";
